402d6c819473a790bdb080afee69003dbaf7ced5
devarturobl <4rthurbl00@gmail.com>
2026-04-21 09:41:16 -0600

    Contador casi terminado

--- Unidad 4/Semana 11/contarauto.py
+++ Unidad 4/Semana 11/contarauto.py
@@ -20,11 +20,44 @@ while True:
 
     area_pts = np.array([[330,216], [frame.shape[1]-80,216], [frame.shape[1]-80, 271], [330,271]])
 
+    imAux = np.zeros(shape=(frame.shape[:2]), dtype=np.uint8)
+    imAux = cv2.drawContours(imAux, [area_pts], -1, (255), -1)
+    image_area = cv2.bitwise_and(frame, frame, mask=imAux)
+
+    # Obtendremos la imagen binaria donde la región en blanco representa
+    # la existencia de movimiento
+    fgmask = fgbg.apply(image_area)
+    fgmask = cv2.morphologyEx(fgmask, cv2.MORPH_OPEN, kernel)
+    fgmask = cv2.morphologyEx(fgmask, cv2.MORPH_CLOSE, kernel)
+    fgmask = cv2.dilate(fgmask, None, iterations=5)
+
+    cnts = cv2.findContours(fgmask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[0]
+    for cnt in cnts:
+        if cv2.contourArea(cnt) > 1500:
+            x, y, w, h = cv2.boundingRect(cnt)
+            cv2.rectangle(frame, (x,y), (x+w,y+h), (0,255,255), 1)
+
+            # Si el auto ha cruzado entre 440 y 460 abierto, se incrementará
+            # en 1 el contador de autos
+            if 440 < (x + w) < 460:
+                car_counter = car_counter + 1
+                cv2.line(frame, (450, 216), (450, 271), (0, 255, 0), 3)
+                # Visualización del conteo de autos
+                cv2.drawContours(frame, [area_pts], -1, (255, 0, 255), 2)
+                cv2.line(frame, (450, 216), (450, 271), (0, 255, 255), 1)
+                cv2.rectangle(frame, (frame.shape[1]-70, 215), (frame.shape[1]-5, 270), (0, 255, 0), 2)
+        
+        
+                
     #visualizar
     cv2.drawContours(frame, [area_pts], -1, (255,0,255), 2)
     cv2.line(frame, (450,216), (450,271), (0,255,255), 1)
+    cv2.rectangle(frame, (frame.shape[1]-70, 215), (frame.shape[1]-5, 270), (0, 255, 0), 2)
+    cv2.putText(frame, str(car_counter), (frame.shape[1]-55, 250),
+    cv2.FONT_HERSHEY_SIMPLEX, 1.2, (0,255,0), 2)
     cv2.imshow('Frame', frame)
-
+    cv2.imshow('FGMASK', fgmask)
+    
     k = cv2.waitKey(1) & 0xFF
     if k == 27:
         break
